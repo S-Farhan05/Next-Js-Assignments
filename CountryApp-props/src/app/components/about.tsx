f9ede7d639c43d94c9c25232d29b27506ef9bfc4
S-Farhan05 <159949925+S-Farhan05@users.noreply.github.com>
2024-10-25 23:32:46 +0500

    Update about.tsx

--- CountryApp-props/src/app/components/about.tsx
+++ CountryApp-props/src/app/components/about.tsx
@@ -3,8 +3,9 @@ import React from "react";
 export default function About(){
 return(
     <>
+    <div id="nav">
     <h1>Made By :<b>Syed Farhan Iqbal</b></h1>
-    <h1>My socials :<a href="https://github.com/S-Farhan05">Github</a></h1>
+    <h1>My socials :<a href="https://github.com/S-Farhan05">Github</a></h1></div>
     </>
 )
 }
